43804bdd65595c7a0081be8d6a2c07f65a4405ff
Charapalli-Tharun <56440485+Tharun000@users.noreply.github.com>
2020-10-01 16:59:57 +0530

    Update sumofn.py

--- sumofn.py
+++ sumofn.py
@@ -5,3 +5,5 @@ def summation_by_formula(n):
 if __name__ == "__main__":
     num = int(input("Enter the number until which you want to find the sum : "))
     print("The sum of first %d numbers is %d." % (num, summation_by_formula(num)))
+
+//WE can use .format() for better understanding.
